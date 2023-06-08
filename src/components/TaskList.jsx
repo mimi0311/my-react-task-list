4e22d67678fa7c36f3e2060a36f794df196f68e2
Miriam Pinto <miriam.pinto0311@gmail.com>
2023-06-07 21:11:18 -0400

    Implementar hooks

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Task from './Task';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function TaskList() {
   const [tasks, setTasks] = useState([
     {name: "Tarea 1", status: true},
     {name: "Tarea 2", status: false},
   ]);
+
+  useEffect(() => {
+    setTasks(JSON.parse(localStorage.getItem("tasks")) || []);
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks])
 
   return (
     <>
